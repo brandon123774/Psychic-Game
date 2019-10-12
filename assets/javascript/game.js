1212e7830b8d42edde78027b15b6e20eff0acdbf
Brandon Shankar <bshankar12@comcast.net>
2019-10-11 22:25:26 -0700

    added script file and edited

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,9 @@ var wins: 0;
 var losses: 0;
 
 document.write("Guess what letter I'm thinking of");
+document.write("Wins: " + wins);
+document.write("Losses: " + losses);
 document.write ("Your Guesses so far: ");
+
+
+
