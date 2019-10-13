b4751c4662bd383ef6bbb921588971a789a1e3ac
Brandon Shankar <bshankar12@comcast.net>
2019-10-12 21:02:49 -0700

    edited HTML doc with div section

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,29 @@
-var wins: 0;
-var losses: 0;
+//created variables that are needed for game
+var wins = 0;
+var losses = 0;
 
+//created alphabet array for letter choices
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//print out main statement
 document.write("Guess what letter I'm thinking of");
+
+//create variables that are to be used from the HTMl doc
+var userInputText = document.getElementById("userinput-text");
+var computerInputText = document.getElementById("computerinput-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+
+// key press input 
+document.onkeyup = function (event) {
+
+}
+
+// print out the number of wins, losses, and number of guesses
 document.write("Wins: " + wins);
 document.write("Losses: " + losses);
-document.write ("Your Guesses so far: ");
+document.write("Your Guesses so far: ");
 
 
 
