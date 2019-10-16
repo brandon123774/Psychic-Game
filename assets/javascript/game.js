ffba8ea312ae803749e730612df6570c07722d95
Brandon Shankar <bshankar12@comcast.net>
2019-10-15 21:07:17 -0700

    properly edited js and html files to make game work as needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 //created variables that are needed for game
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 9;
+var lettersClicked = []
 
 //created alphabet array for letter choices
 var computerChoice = "abcdefghijklmnopqrstuvwxyz";
@@ -12,37 +13,65 @@ var computerInputText = document.getElementById("computerinput-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+
+
+// Computer has a random choice prior to the user's input
+var computerInput = ""
 
 // key press input 
 document.onkeyup = function (event) {
 
-     // which key was pressed is listed here
-     var userInput = event.key;
-
-    // Computer has a random choice prior to the user's input
-    var computerInput = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    console.log(computerInput)
-   
-  // loop through the array to see if user guess is correct
-  for (var i = 0; i < computerChoice.length; i++)   {
-      if (userInput == computerChoice)
-      wins ++;
-      guessesLeft--;
-     } 
-     else {
-        losses++;
-    }
+  // which key was pressed is listed here
+  var userInput = event.key;
+  console.log(userInput, computerInput)
+  lettersClicked.push(userInput)
+  guessesLeft--;
+  if (userInput === computerInput) {
+    wins++;
+    console.log("WIN")
+    restart()
   }
+  else if (guessesLeft === 0) {
+    losses++;
+    console.log("LOST")
+    restart()
+  }
+  else {
+    console.log(wins, losses)
+    console.log(lettersClicked)
+    console.log(guessesLeft)
+    guessesLeftText.innerHTML = guessesLeft;
+    guessesText.innerHTML = lettersClicked;
+  }
+}
 
-    // Display the user's guesses left, wins/losses.
-  
-    // document.getElementById("wins-text").innerHTML = "Wins: " + wins;
-    // document.getElementById("losses-text").innerHTML = "Losses: " + losses;
-    // document.getElementById("triesMax-text").innerHTML = "Guesses Left: " + guessesLeft;
 
 
-};
+// loop through the array to see if user guess is correct
 
 
+
+// Display the user's guesses left, wins/losses.
+
+// document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+// document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+// document.getElementById("triesMax-text").innerHTML = "Guesses Left: " + guessesLeft;
+
+function restart() {
+  console.log("RESTART")
+  winsText.innerHTML = wins;
+  lossesText.innerHTML = losses;
+
+  computerInput = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+  console.log(computerInput)
+  guessesLeft = 9;
+  lettersClicked = []
+  guessesLeftText.innerHTML = guessesLeft;
+  guessesText.innerHTML = lettersClicked;
+}
+
+
+restart()
 
 
